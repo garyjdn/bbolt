7f30187aa7be4a95dbdfb393acc49665e86cd1b4
calvine <calvine@assetdata.io>
2022-03-03 17:36:57 +0700

    fix package identifier

--- android/src/main/java/com/example/bbolt/MainActivity.java
+++ android/src/main/java/com/example/bbolt/MainActivity.java
@@ -1,4 +1,4 @@
-package io.flutter.plugins.firebasecoreexample;
+package com.example.bbolt;
 
 import io.flutter.embedding.android.FlutterActivity;
 import io.flutter.embedding.engine.FlutterEngine;
