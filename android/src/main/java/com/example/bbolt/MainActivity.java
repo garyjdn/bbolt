90b6f8df1c0e99435e4503198366c40aa62fbb7b
calvine <calvine@assetdata.io>
2022-03-03 16:36:28 +0700

    test new plugin

---
+++ android/src/main/java/com/example/bbolt/MainActivity.java
@@ -0,0 +1,9 @@
+package io.flutter.plugins.firebasecoreexample;
+
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugins.firebase.core.FirebaseCorePlugin;
+
+public class MainActivity extends FlutterActivity {
+    
+}
